9e72372cf35d77e8ff835a2c1d444e85a2b9ee71
Austin Jess <ajess33@gmail.com>
2019-05-24 09:43:17 -0500

    added more tests to check for edge cases

--- challenges/arrayBinarySearch/array-binary-search.test.js
+++ challenges/arrayBinarySearch/array-binary-search.test.js
@@ -1,19 +1,22 @@
 const binarySearch = (arr, val) => {
+  if (!Array.isArray(arr)) {
+    return `You need to pass in an array in order to do a binary search`;
+  } else if (arr.length === 1) {
+    return 'Cannot do a binary search on one element';
+  }
+
   let start = 0;
   let end = arr.length - 1;
-
   let mid = Math.ceil((start + end) / 2);
+  let loopGuard = 0;
 
-  while (arr[mid] !== val) {
+  while (arr[mid] !== val && loopGuard++ < 1000) {
     if (start === end - 1 && (start !== val && end !== val)) {
       return -1;
     } else if (arr[mid] > val) {
-      console.log(start, mid, end);
       end = mid - 1;
       mid = Math.ceil((start + end) / 2);
     } else if (arr[mid] < val) {
-      console.log(start, mid, end);
-
       start = mid + 1;
       mid = Math.ceil((start + end) / 2);
     }
@@ -21,9 +24,32 @@ const binarySearch = (arr, val) => {
   return mid;
 };
 
-const testArray = [1, 3, 5, 7, 9, 11];
+let testArray = [];
+for (let i = 0; i < 10000; i++) {
+  testArray.push(i);
+}
 
 describe('The binary search function', () => {
-  it('should return an index if it finds a match', () => {});
-  expect(binarySearch([testArray], 3)).toBe(2);
+  it('should return an index if it finds a match', () => {
+    expect(binarySearch([0, 1, 2, 3, 4, 5], 1)).toBe(1);
+  });
+
+  it('should return an index for an array of any length', () => {
+    expect(binarySearch(testArray, 4)).toBe(4);
+  });
+
+  it('should return a message if array is of length 1', () => {
+    console.log(binarySearch([1], 1));
+    expect(binarySearch([1], 1)).toBe(
+      `Cannot do a binary search on one element`
+    );
+  });
+
+  it(`should return a message if array isn't passed in`, () => {
+    expect(binarySearch(1, 1)).toBe(
+      `You need to pass in an array in order to do a binary search`
+    );
+  });
 });
+
+// WRITE MORE TESTS
