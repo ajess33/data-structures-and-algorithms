9d4ba00c2e7b7001a6f824ec0737ea98b4744f14
Austin Jess <ajess33@gmail.com>
2019-05-28 10:35:30 -0500

    initial commit

---
+++ challenges/ll-Insertions/ll-Insertions.test.js
@@ -0,0 +1,91 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertMany(arr) {
+    if (!arr) {
+      return `You must input an array consisting of at least one value`;
+    }
+    arr.forEach((value) => {
+      const newNode = new Node(value);
+      if (!this.head) {
+        this.head = newNode;
+      } else {
+        newNode.next = this.head;
+        this.head = newNode;
+      }
+    });
+  }
+
+  insert(value) {
+    if (!value) {
+      return `You must input a value`;
+    }
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  includes(searchQuery) {
+    if (!searchQuery) {
+      return `You must input a search query`;
+    }
+    let current = this.head;
+
+    while (current) {
+      if (current.value === searchQuery) {
+        return true;
+      } else {
+        current = current.next;
+        return false;
+      }
+    }
+  }
+
+  print() {
+    if (!this.head) {
+      return `You have to insert a node first!`;
+    } else {
+      let current = this.head;
+      let stringNodes = '';
+      while (current) {
+        stringNodes += current.value + ' ';
+        current = current.next;
+      }
+      return stringNodes;
+    }
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+/*
+TESTS
+1. Can successfully add a node to the end of the linked list
+2. Can successfully add multiple nodes to the end of a linked list
+3. Can successfully insert a node before a node located i the middle of a linked list
+4. Can successfully insert a node before the first node of a linked list
+5. Can successfully insert after a node in the middle of the linked list
+6. Can successfully insert a node after the last node of the linked list
+*/
+
+describe('The linked list', () => {
+  const list = new LinkedList();
+  list.insert('first value');
+  list.insert('second value');
+
+  it('instantiates an empty array to begin', () => {
+    const myLinkedList = new LinkedList();
+    expect(myLinkedList).toBeDefined();
+  });
+});
