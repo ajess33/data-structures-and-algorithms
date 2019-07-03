3d8ef59aad29f624f09a2cd2a994e6591b6be88e
Austin Jess <ajess33@gmail.com>
2019-07-03 13:44:20 -0500

    all methods are implemented

--- data-structures/hash-table/hashtable.js
+++ data-structures/hash-table/hashtable.js
@@ -1,23 +1,61 @@
+const LinkedList = require('../linkedList/linked-list.test');
+
 class HashTable {
-  constructor() {
-    this.keys = [];
-    this.values = [];
+  constructor(count) {
+    this.data = [];
+    this.bucketCount = count || 1024;
   }
 
-  add(key, value) {
+  add(obj) {
     // takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling collisions as need
+    if (!obj) return `Please provide and object`;
+    let index = this.hash(Object.keys(obj)[0]);
+    if (!this.data[index]) {
+      this.data[index] = new LinkedList();
+    }
+    // insert is from linkedlist constructor
+    this.data[index].insert(obj);
+    return this.data[index].head;
   }
 
   get(key) {
     // takes in the key and returns the value from the table.
+    console.log(key);
+    if (!key) return `Please provide a key`;
+    let current;
+    let index = this.hash(key);
+    if (this.data[index].head) {
+      current = this.data[index].head;
+    } else {
+      current = null;
+    }
+    while (current) {
+      if (current.value[key]) return current.value;
+      current = current.next;
+    }
+    return current;
   }
 
   contains(key) {
     // takes in the key and returns a boolean, indicating if the key exists in the table already.
+    if (!key) return `Please provide a key`;
+    let value = this.get(key);
+    if (!value) {
+      return false;
+    }
+    return true;
+    // const boolean = !!this.get(key);
+    // return boolean;
   }
 
-  hash() {
-    // takes in an arbitrary key and returns an index in the collection.
+  hash(key) {
+    if (!key) return `Please provide a key`;
+    let hash = key
+      .toString()
+      .split('')
+      .map((char) => char.charCodeAt(0))
+      .reduce((acc, cur) => acc + cur);
+    return Math.floor((hash * 599) / this.bucketCount);
   }
 }
 
--- data-structures/hash-table/hashtable.test.js
+++ data-structures/hash-table/hashtable.test.js
@@ -2,6 +2,28 @@ const HashTable = require('./hashtable');
 
 describe('The HashTable Class', () => {
   it('can produce a hash code', () => {
-    expect();
+    let testTable = new HashTable(1024);
+
+    expect(testTable.hash('cat')).toBe(182);
+    expect(testTable.hash('austin')).toBe(386);
+    expect(testTable.hash()).toBe(`Please provide a key`);
+  });
+
+  it('can add an object to the hash table', () => {
+    let testTable = new HashTable(1024);
+    let test = testTable.add({ cat: 100 });
+    expect(test.value).toEqual({ cat: 100 });
+  });
+
+  it('can get an object from the hash table', () => {
+    let testTable = new HashTable(1024);
+    testTable.add({ cat: 100 });
+    expect(testTable.get('cat')).toEqual({ cat: 100 });
+  });
+
+  it('returns a boolean referencing if the key is in the table or not', () => {
+    let testTable = new HashTable();
+    testTable.add({ dog: 100 });
+    expect(testTable.contains('dog')).toBeTruthy();
   });
 });
