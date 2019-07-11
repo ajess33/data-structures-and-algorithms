51178d6df88421cbac35ed1689a7b2daa103e071
Austin Jess <ajess33@gmail.com>
2019-07-10 19:37:51 -0500

    added test

--- challenges/left-join/left-join.test.js
+++ challenges/left-join/left-join.test.js
@@ -1,10 +1,6 @@
 const HashTable = require('../../data-structures/hash-table/hashtable');
 const leftJoin = require('./left-join');
 
-// [ [ 'fond', 'enamoured', 'averse' ],
-// [ 'wrath', 'anger', 'delight' ],
-// [ 'outfit', 'garb', null ] ]
-
 describe('The LeftJoin Function', () => {
   it('should return a left join from two hashmaps', () => {
     let result = leftJoin(hash1, hash2);
